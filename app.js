53d88f20f34e9a2dacf7b70c52985129f1d8ffe2
Luka Sarc <sarc.luka@gmail.com>
2015-03-23 09:18:13 +0100

    prvi commit

---
+++ app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var friKatedreApp = angular.module('friKatedre', [
+  'ngRoute',
+  'friKatedreCtrls'
+]);
+        
+friKatedreApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            controller: 'kazaloCtrl',
+            templateUrl: 'views/kazalo.html'
+        })
+        .when('programska-oprema', {
+            controller: 'programskaOpremaCtrl',
+            templateUrl: 'views/programskaOprema.html'
+        })
+        .when('racunalniska-logika', {
+            controller: 'racunalniskaLogikaCtrl',
+            templateUrl: 'views/racunalniskaLogika.html'
+        })
+        .when('informatika', {
+            controller: 'informatikaCtrl',
+            templateUrl: 'views/informatika.html'
+        })
+        .when('teoreticno-racunalnistvo', {
+            controller: 'teoreticnoRacunalnistvoCtrl',
+            templateUrl: 'views/teoreticnoRacunalnistvo.html'
+        })
+        .when('umetna-inteligenca', {
+            controller: 'umetnaInteligencaCtrl',
+            templateUrl: 'views/umetnaInteligenca.html'
+        })
+        .when('matematika', {
+            controller: 'matematikaCtrl',
+            templateUrl: 'views/matematika.html'
+        })
+        .otherwise({redirectTo: '/'});
+}]);
+
+friKatedreApp.run( [ '$rootScope', '$location', function( $rootScope, $location ) {
+    $rootScope.isActive = function( url ) {
+        if ( $location.path().indexOf( url ) !== -1 ) return true;
+        else return false;
+    };
+}]);
---
+++ app/controllers.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var friKatedreCtrls = angular.module('friKatedreCtrls', []);
+
+friKatedreCtrls.controller('kazaloCtrl', ['$scope', function ($scope) {
+        
+}]);
+
+friKatedreCtrls.controller('programskaOpremaCtrl', ['$scope', function ($scope) {
+        
+}]);
+
+friKatedreCtrls.controller('racunalniskaLogikaCtrl', ['$scope', function ($scope) {
+        
+}]);
+
+friKatedreCtrls.controller('informatikaCtrl', ['$scope', function ($scope) {
+        
+}]);
+
+friKatedreCtrls.controller('teoreticnoRacunalnistvoCtrl', ['$scope', function ($scope) {
+        
+}]);
+
+friKatedreCtrls.controller('umetnaInteligencaCtrl', ['$scope', function ($scope) {
+        
+}]);
+
+friKatedreCtrls.controller('matematikaCtrl', ['$scope', function ($scope) {
+        
+}]);
